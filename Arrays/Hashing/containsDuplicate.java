fc7214ddf472a1ce3545a29f68e94c4f6361e41a
Karan Singh Rajput <karansinghrajput2022@vitbhopal.ac.in>
2026-06-04 23:15:08 +0530

    Add Contains Duplicate solution

---
+++ Arrays/Hashing/containsDuplicate.java
@@ -0,0 +1,18 @@
+package Arrays.Hashing;
+
+import java.util.HashSet;
+
+public class containsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
