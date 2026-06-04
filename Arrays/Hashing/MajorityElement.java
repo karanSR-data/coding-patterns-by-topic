0b986c1e83f3bc35292ca20fef0f1aac5c85e3b2
Karan Singh Rajput <karansinghrajput2022@vitbhopal.ac.in>
2026-06-04 23:13:19 +0530

    Add Majority Element solution

---
+++ Arrays/Hashing/MajorityElement.java
@@ -0,0 +1,18 @@
+package Arrays.Hashing;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for(int num : nums) {
+            if(count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
