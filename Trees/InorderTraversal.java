336ad9b90b07ed4b03ad8170007b4001a9f568e1
Karan Singh Rajput <karansinghrajput2022@vitbhopal.ac.in>
2026-06-05 14:34:29 +0530

    Add Inorder Traversal of Binary Tree

---
+++ Trees/InorderTraversal.java
@@ -0,0 +1,22 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(TreeNode root, List<Integer> res) {
+        if(root == null) return;
+
+        dfs(root.left, res);
+        res.add(root.val);
+        dfs(root.right, res);
+    }
+}
