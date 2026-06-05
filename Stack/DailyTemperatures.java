445afe62305efa02bab7615dc355f01927c89688
Karan Singh Rajput <karansinghrajput2022@vitbhopal.ac.in>
2026-06-05 14:34:08 +0530

    Add Daily Temperatures monotonic stack solution

---
+++ Stack/DailyTemperatures.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.isEmpty() &&
+                  temperatures[i] > temperatures[st.peek()]) {
+
+                int idx = st.pop();
+                res[idx] = i - idx;
+            }
+
+            st.push(i);
+        }
+
+        return res;
+    }
+}
